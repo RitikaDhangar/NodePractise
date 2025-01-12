93bbf17a124b04dc6d19082ba8290bb7d56711fd
dhangarritika <dhangarritika90@gmail.com>
2025-01-12 20:49:31 +0530

    delete code

--- routes/store.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const messageController = require("../controller/msgs");
-router.post("/Store-message", messageController.addMessages);
-router.get("/show-message", messageController.allMessages);
-module.exports = router;
--- utils/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("shoppingapp", "root", "ritika91@", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
