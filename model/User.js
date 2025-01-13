c26de82d103bd88f98a4947767b359017087023e
Ritika Dhangar <Ritika Dhangar>
2025-01-13 18:22:22 +0530

    add code

---
+++ model/User.js
@@ -0,0 +1,25 @@
+const Sequelize=require('sequelize')
+const sequelize = require('../utils/database')
+
+const User = sequelize.define("user", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey:true,
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    useremail: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true 
+    },
+    userpassword: {
+        type: Sequelize.STRING,
+        allowNull:false
+    }
+})
+module.exports=User
