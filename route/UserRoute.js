e00569c7e2217feaaf916a84e00f28bf5d9eda3e
Ritika Dhangar <Ritika Dhangar>
2025-01-15 12:17:01 +0530

    add code

--- route/UserRoute.js
+++ route/UserRoute.js
@@ -3,5 +3,4 @@ const router = express.Router();
 const UserController=require('../controller/UserController')
 router.post('/add-user', UserController.postuserInfo);
 router.post('/login-user', UserController.loginUser);
-router.get('/fetch-allusers', UserController.getAllUsers);
 module.exports=router
